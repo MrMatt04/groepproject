e48b9cce8451ad860f317d4da1b6fbd66155a4a2
Mathieu <mathieuvantuil2000@gmail.com>
2023-03-22 12:58:51 +0100

    Push 2 M

---
+++ plugin/testimonial.php
@@ -0,0 +1,10 @@
+<?php
+/**
+* Plugin Name: Testimonial
+* Plugin URI: gluwebURL
+* Description: hier kun je testimonials maken
+Version: 1.0.0
+Author: project :)
+Author URI: gluwebURL
+License: GPL-2.0+
+*/
