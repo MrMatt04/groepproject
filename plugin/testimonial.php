b0b067569ef4920a210f6cbc01f08b2d7c246bb3
Mathieu <mathieuvantuil2000@gmail.com>
2023-03-22 14:13:55 +0100

    commit m

--- plugin/testimonial.php
+++ plugin/testimonial.php
@@ -8,3 +8,25 @@ Author: project :)
 Author URI: gluwebURL
 License: GPL-2.0+
 */
+
+// Voeg html toe onderin de pagina.
+add_action('get_header','mybox');
+function mybox(){
+  echo '<div class="infobox">Hier komt wat informatie</div>';
+}
+// Voeg styling toe in de <head>.
+add_action('get_header','mystyles');
+function mystyles(){
+  echo '<style>
+  .infobox{
+     background-color:black;
+   width:100%;
+   height:54px;
+   color:white;
+   display:flex;
+   justify-content:center;
+   align-items:center;
+ }
+ </style>';
+}
+?>
